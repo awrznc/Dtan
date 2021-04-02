a82b071d57cc329c4597f1758a9dbb89f58a87b0
awrznc <49797644+awrznc@users.noreply.github.com>
2021-04-02 23:31:46 +0900

    Remove local paths.

--- example/dtpn/bin/dtpn.bat
+++ example/dtpn/bin/dtpn.bat
@@ -3,11 +3,11 @@
 
 cd %*
 
-for /F "usebackq" %%i in (`C:\Users\orz\Desktop\git\Dtan\build\bin\dtansp.exe`) do set dtan_path=%%~i
+for /F "usebackq" %%i in (`dtansp.exe`) do set dtan_path=%%~i
 
 for %%1 in ("%dtan_path:~0,-1%") do set print_string=%%~nx1
 
 if "%ERRORLEVEL%" equ "0" if "%DTPN_SYMBOL%" neq "%dtan_path%" (
-  C:\Users\orz\Desktop\git\Dtan\build\bin\dtan.exe "      %print_string%      "
+  dtan.exe "      %print_string%      "
   set DTPN_SYMBOL=%dtan_path%
 )
